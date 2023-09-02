6505472d18f4efe82920c7828a709c78868a95af
Zeta <zeta0134@reploid.cafe>
2023-09-02 15:32:39 -0400

    Create some basic test tiles, a tileset, and a map

---
+++ art/level_tileset.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="level_tileset" tilewidth="16" tileheight="16" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="tile_index" type="int" value="0"/>
+  </properties>
+  <image width="16" height="16" source="tiles/air.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="tile_index" type="int" value="1"/>
+  </properties>
+  <image width="16" height="16" source="tiles/wall.png"/>
+ </tile>
+</tileset>
